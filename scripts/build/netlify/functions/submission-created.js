8b4472efb256c3d40df0aef62cf8db5eb68efa29
conor o shea <conoshea12@gmail.com>
2021-04-20 14:25:45 +0100

    test

--- scripts/build/netlify/functions/submission-created.js
+++ scripts/build/netlify/functions/submission-created.js
@@ -1,6 +1,7 @@
 // submission-created.js
 exports.handler = async function (event, context) {
   console.log(JSON.parse(event.body));
+  console.log(context);
   return {
     statusCode: 200,
   };
