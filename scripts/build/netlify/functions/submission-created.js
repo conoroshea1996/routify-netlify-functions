91eaf3236c6a5947d1c6dbc876fa74fb5e77a824
conor o shea <conoshea12@gmail.com>
2021-04-20 15:08:28 +0100

    -testing

--- scripts/build/netlify/functions/submission-created.js
+++ scripts/build/netlify/functions/submission-created.js
@@ -1,14 +1,13 @@
 // submission-created.js
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
   console.log(JSON.parse(event.body));
   console.log(context);
 
-  const result = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  let x = await result.json();
-
-  console.log(x);
+  // const result = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  // let x = await result.json();
+  // console.log(x);
 
   return {
     statusCode: 200,
